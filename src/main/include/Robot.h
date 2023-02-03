b3f56acba67ad0502cf8ab8c1a41fb9d7120a922
omzz15 <drompatel5@gmail.com>
2023-02-03 17:46:05 -0500

    stoof

---
+++ src/main/include/Robot.h
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc/XboxController.h>
+
+#include <ctre/phoenix/motorcontrol/can/WPI_VictorSPX.h>
+
+class Robot : public frc::TimedRobot {
+public:
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TestInit() override;
+  void TestPeriodic() override;
+  void SimulationInit() override;
+  void SimulationPeriodic() override;
+
+private:
+  frc::XboxController m_controller {0};
+  ctre::phoenix::motorcontrol::can::WPI_VictorSPX m_motor {1};
+
+  double m_power = 0.0;
+  constexpr static double m_ramp = 0.05;
+};
